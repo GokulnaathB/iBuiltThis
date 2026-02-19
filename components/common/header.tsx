ae716462cf290c8f372b47bb0a5a42932844bcaa
gokul-2101 <112502003@smail.iitpkd.ac.in>
2026-02-19 12:20:40 +0530

    made header responsive

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -21,8 +21,8 @@ const Logo = () => {
 };
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50 border-b bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60">
-      <div className="wrapper px-12">
+    <header className="sticky top-0 z-50 border-b bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60 ">
+      <div className="wrapper px-1 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
           <Logo />
           <nav className="flex items-center gap-1">
